13b720b5981dd971d4194ef6bdf371cc466f52f6
ZRMYDYCG <547471919@qq.com>
2024-10-28 11:19:04 +0800

    feat: 渲染菜单

--- src/Layout/components/layout-menu.vue
+++ src/Layout/components/layout-menu.vue
@@ -13,23 +13,25 @@ import type { IMenubarList } from '@/config/interface'
 const userStore = useUserStore()
 
 // 过滤隐藏的菜单，并提取单条的子菜单
-const filterMenubar = (menuList:IMenubarList[]) => {
-    const f = (menuList:IMenubarList[]) => {
-        let arr:IMenubarList[] = []
-        menuList.filter(v => !v.meta.hidden).forEach(v => {
-            let child = v.children && v.children.filter(v => !v.meta.hidden)
-            let currentItem = v
-            if(!v.meta.alwaysShow && child && child.length === 1) {
-                [currentItem] = child
-            }
-            arr.push(currentItem)
-            if(currentItem.children && currentItem.children.length > 0) {
-                arr[arr.length - 1].children = f(currentItem.children)
-            }
-        })
-        return arr
-    }
-    return f(menuList)
+const filterMenubar = (menuList: IMenubarList[]) => {
+  const f = (menuList: IMenubarList[]) => {
+    let arr: IMenubarList[] = []
+    menuList
+      .filter(v => !v.meta.hidden)
+      .forEach(v => {
+        let child = v.children && v.children.filter(v => !v.meta.hidden)
+        let currentItem = v
+        if (!v.meta.alwaysShow && child && child.length === 1) {
+          ;[currentItem] = child
+        }
+        arr.push(currentItem)
+        if (currentItem.children && currentItem.children.length > 0) {
+          arr[arr.length - 1].children = f(currentItem.children)
+        }
+      })
+    return arr
+  }
+  return f(menuList)
 }
 
 const filterMenuListData = filterMenubar(userStore.menubar.menuList)
@@ -39,7 +41,12 @@ userStore.setRoutes(filterMenuListData)
 <template>
   <el-scrollbar style="width: 100%">
     <el-menu :default-active="1">
-      <layout-menu-item :menu-list="userStore.menubar.menuList"></layout-menu-item>
+      <layout-menu-item
+        v-for="(item, index) in filterMenuListData"
+        :key="item.path"
+        :index="index"
+        :menu-list="item"
+      ></layout-menu-item>
     </el-menu>
   </el-scrollbar>
 </template>
