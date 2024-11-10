ee8d814bcb2000699b8dd2c2b080233d44f897cb
ZRMYDYCG <547471919@qq.com>
2024-11-10 18:04:49 +0800

    feat: 发送并接收消息

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -1,6 +1,6 @@
 <!--
  * @Author: ZRMYDYCG
- * @Date: 2024-10
+ * @Date: 2024-11
  * @LastEditors: ZRMYDYCG
  * @LastEditTime: 2024-11
  * @Description:
@@ -12,15 +12,15 @@
     </div>
     <div class="w-[250px] bg-yellow-200 h-full flex flex-col">
       <div ref="headerRef" class="flex pt-5 px-2">
-        <el-input></el-input>
-        <el-button></el-button>
+        <el-input v-model="message" placeholder="输入消息..."></el-input>
+        <el-button type="primary" @click="sendMessage">发送</el-button>
       </div>
       <el-scrollbar class="h-100%">
-        <div class="flex border-b py-2 px-1 gap-3">
+        <div v-for="(msg, index) in messages" :key="index" class="flex border-b py-2 px-1 gap-3">
           <div class="bg-gray-100 w-[60px] h-[60px] rounded-full"></div>
           <div class="flex flex-col justify-center">
             <div>一勺</div>
-            <div class="text-gray-400 text-sm">吃饭了吗?</div>
+            <div class="text-gray-400 text-sm">{{ msg }}</div>
           </div>
         </div>
       </el-scrollbar>
@@ -30,18 +30,32 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue'
-// import {} from '@/api/modules/system'
+import { ref, onMounted } from 'vue'
 import socket from 'socket.io-client'
+
 const io = socket('ws://127.0.0.1:3000')
+const message = ref('') // 用于绑定输入框的消息
+const messages = ref<string[]>([]) // 用于存储接收到的消息
 
-try {
-  io.on('connection', (client) => {
-    console.log('客户端连接成功', client)
+onMounted(() => {
+  io.on('getMsg', (data: string) => {
+    console.log('接收到的消息：', data)
+    messages.value.push(data) // 将接收到的消息添加到 messages 数组
   })
-} catch (error) {}
 
-onMounted(() => {})
+  // 发送初始化消息
+  setTimeout(() => {
+    io.emit('send', '你好，我是客户端发的消息！')
+  }, 3000)
+})
+
+// 发送消息的函数
+const sendMessage = () => {
+  if (message.value.trim()) {
+    io.emit('send', message.value) // 发送用户输入的消息
+    message.value = '' // 清空输入框
+  }
+}
 </script>
 
 <style lang="scss" scoped>
