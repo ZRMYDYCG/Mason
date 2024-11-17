a434dbae369651977d76bdf0cb703105cbbba7e5
ZRMYDYCG <547471919@qq.com>
2024-11-18 01:28:01 +0800

    feat: Encryption of the password strength check component

--- src/views/user/components/verify-password-strength.vue
+++ src/views/user/components/verify-password-strength.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import { watch } from 'vue'
+import { watch, ref } from 'vue'
 
-enum PasswordStrength {
-  Weakest = 0,
-  Weak = 1,
-  Medium = 2,
-  Strong = 3,
-  VeryStrong = 4
+const PasswordStrength = {
+  Weakest: '极弱',
+  Weak: '弱',
+  Medium: '中',
+  Strong: '强',
+  VeryStrong: '非常强'
 }
 
 interface IProps {
@@ -14,22 +14,88 @@ interface IProps {
 }
 
 const props = defineProps<IProps>()
+const emits = defineEmits(['update:passwordStrength'])
+
+const passwordStrength = ref(undefined as unknown as number)
+
+function calculatePasswordStrength(password: string): number {
+  const length = password.length
+  const hasUpperCase = /[A-Z]/.test(password)
+  const hasLowerCase = /[a-z]/.test(password)
+  const hasNumber = /\d/.test(password)
+  const hasSpecialChar = /[\W_]/.test(password)
+
+  let strength = 0
+
+  // 基于长度的基本评分
+  if (length < 6) {
+    strength = 0 // 极弱
+  } else if (length >= 6 && length < 10) {
+    strength = 1 // 弱
+  } else if (length >= 10 && length < 14) {
+    strength = 2 // 中
+  } else if (length >= 14 && length < 18) {
+    strength = 3 // 强
+  } else {
+    strength = 4 // 非常强
+  }
+
+  // 根据不同的条件增加额外的强度分
+  if (hasUpperCase) strength++
+  if (hasLowerCase) strength++
+  if (hasNumber) strength++
+  if (hasSpecialChar) strength++
+
+  // 最大强度不超过 4
+  return Math.min(strength, 4)
+}
 
 watch(
   () => props.password,
-  (newVal, oldVal) => {
-    console.log(newVal, oldVal)
+  (newVal) => {
+    passwordStrength.value = calculatePasswordStrength(newVal)
+    emits('update:passwordStrength', passwordStrength.value)
   }
 )
 </script>
 
 <template>
   <div class="flex w-full gap-2">
-    <div class="flex-1 h-2 bg-red-600"></div>
-    <div class="flex-1 h-2 bg-green-200"></div>
-    <div class="flex-1 h-2 bg-green-300"></div>
-    <div class="flex-1 h-2 bg-green-400"></div>
-    <div class="flex-1 h-2 bg-green-500"></div>
+    <div class="flex-1 flex flex-col items-center">
+      <div
+        class="w-full h-2 bg-gray-100"
+        :class="passwordStrength > 0 ? 'bg-red-600' : 'bg-gray-100'"
+      ></div>
+      <div>{{ PasswordStrength.Weakest }}</div>
+    </div>
+    <div class="flex-1 flex flex-col items-center">
+      <div
+        class="w-full h-2 bg-gray-100"
+        :class="passwordStrength > 1 ? 'bg-green-200' : 'bg-gray-100'"
+      ></div>
+      <div>{{ PasswordStrength.Weak }}</div>
+    </div>
+    <div class="flex-1 flex flex-col items-center">
+      <div
+        class="w-full h-2 bg-gray-100"
+        :class="passwordStrength >= 2 ? 'bg-green-300' : 'bg-gray-100'"
+      ></div>
+      <div>{{ PasswordStrength.Medium }}</div>
+    </div>
+    <div class="flex-1 flex flex-col items-center">
+      <div
+        class="w-full h-2 bg-gray-100"
+        :class="passwordStrength >= 3 ? 'bg-green-400' : 'bg-gray-100'"
+      ></div>
+      <div>{{ PasswordStrength.Strong }}</div>
+    </div>
+    <div class="flex-1 flex flex-col items-center">
+      <div
+        class="w-full h-2 bg-gray-100"
+        :class="passwordStrength >= 4 ? 'bg-green-500' : 'bg-gray-100'"
+      ></div>
+      <div>{{ PasswordStrength.VeryStrong }}</div>
+    </div>
   </div>
 </template>
 
