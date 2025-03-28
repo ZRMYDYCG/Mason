2bfe371a45d0aefd7a24b8a9680d4484aa0f890f
ZRMYDYCG <547471919@qq.com>
2025-03-28 14:55:18 +0800

    feat: ✨️初始化三次封装el-table结构

--- src/components/CustomTable/index.vue
+++ src/components/CustomTable/index.vue
@@ -1,5 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import MasonTable from '../Table/index.vue'
 
-<template></template>
+const MasonTableRef = ref<InstanceType<typeof MasonTable> | null>(null)
+</script>
+
+<template>
+  <div class="mason-custom-table">
+    <MasonTable ref="MasonTableRef">
+      <slot></slot>
+      <template #otherOperate="{ row: $row }">
+        <slot name="otherOperate" :row="$row"></slot>
+      </template>
+      <template #empty>
+        <slot name="empty"></slot>
+      </template>
+    </MasonTable>
+  </div>
+</template>
 
 <style scoped lang="scss"></style>
