9c1a0632d4f258025d0c800b464ecf8ed440bf0a
ZRMYDYCG <547471919@qq.com>
2025-05-23 08:54:22 +0800

    cli: 新增路由 WIP

--- cli/src/index.js
+++ cli/src/index.js
@@ -2,6 +2,7 @@ import { program } from 'commander'
 import { gitHelperCommand } from './commands/gitHelper.js'
 import { createProjectCommandConfig } from './commands/createProject.js'
 import { genComponentCommandConfig } from './commands/createComponent.js'
+import { genViewCommandConfig } from './commands/createView.js'
 
 // 初始化所有命令
 const initializeCommands = () => {
@@ -13,6 +14,7 @@ const initializeCommands = () => {
 
   // 注册 gen 命令组
   genComponentCommandConfig()
+  genViewCommandConfig()
 
   // 最后解析参数
   program.parse(process.argv)
