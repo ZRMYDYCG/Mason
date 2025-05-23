67ea98a03ca6c95e55aad8811165e9f7f7629393
ZRMYDYCG <547471919@qq.com>
2025-05-23 08:42:34 +0800

    cli: 新增创建组件工具

--- cli/src/index.js
+++ cli/src/index.js
@@ -1,14 +1,18 @@
 import { program } from 'commander'
 import { gitHelperCommand } from './commands/gitHelper.js'
 import { createProjectCommandConfig } from './commands/createProject.js'
+import { genComponentCommandConfig } from './commands/createComponent.js'
 
 // 初始化所有命令
-export const initializeCommands = () => {
+const initializeCommands = () => {
   // 注册 git 命令组
   gitHelperCommand()
 
   // 注册 create 命令组
   createProjectCommandConfig()
+
+  // 注册 gen 命令组
+  genComponentCommandConfig()
 
   // 最后解析参数
   program.parse(process.argv)
