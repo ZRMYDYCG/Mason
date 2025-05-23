958d9388e15a3fa808f00d495587319a2ec0ad1e
ZRMYDYCG <547471919@qq.com>
2025-05-23 08:24:51 +0800

    cli: 新增创建项目基础模板

--- cli/src/index.js
+++ cli/src/index.js
@@ -1,10 +1,14 @@
 import { program } from 'commander'
 import { gitHelperCommand } from './commands/gitHelper.js'
+import { createProjectCommandConfig } from './commands/createProject.js'
 
 // 初始化所有命令
 export const initializeCommands = () => {
   // 注册 git 命令组
   gitHelperCommand()
+
+  // 注册 create 命令组
+  createProjectCommandConfig()
 
   // 最后解析参数
   program.parse(process.argv)
