1b645c3128ef725118d85cafbb9dcf1db2f1a532
ZRMYDYCG <547471919@qq.com>
2025-05-23 09:04:47 +0800

    cli: 新增统计代码量 statistics 指令

--- cli/src/index.js
+++ cli/src/index.js
@@ -3,6 +3,7 @@ import { gitHelperCommand } from './commands/gitHelper.js'
 import { createProjectCommandConfig } from './commands/createProject.js'
 import { genComponentCommandConfig } from './commands/createComponent.js'
 import { genViewCommandConfig } from './commands/createView.js'
+import { statisticsCommand } from './commands/statistics.js'
 
 // 初始化所有命令
 const initializeCommands = () => {
@@ -15,6 +16,12 @@ const initializeCommands = () => {
   // 注册 gen 命令组
   genComponentCommandConfig()
   genViewCommandConfig()
+
+  // 注册 statistics 命令组
+  program
+    .command('statistics')
+    .description('统计项目中的字符数和代码行数')
+    .action(statisticsCommand)
 
   // 最后解析参数
   program.parse(process.argv)
