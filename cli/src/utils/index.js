ab30a633cd2994cb6b2e452e1083a88808ebf1f0
resilience <547471919@qq.com>
2026-02-14 18:19:42 +0800

    chore: 迁移脚手架

--- cli/src/utils/index.js
+++
@@ -1,7 +0,0 @@
-export function toLowerCase(value) {
-  return value.slice(0, 1).toLowerCase() + value.slice(1)
-}
-
-export function toUpperCase(value) {
-  return value.slice(0, 1).toUpperCase() + value.slice(1)
-}
